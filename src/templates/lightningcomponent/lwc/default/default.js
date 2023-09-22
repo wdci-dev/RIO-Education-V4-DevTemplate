719c1ccce7dc86ddd0fad15ba695b28a1e3cdc9b
WDCi Dev <dev@wdcigroup.net>
2023-09-22 10:11:20 +0800

    updated lwc template

--- src/templates/lightningcomponent/lwc/default/default.js
+++ src/templates/lightningcomponent/lwc/default/default.js
@@ -7,7 +7,12 @@
  */
 import { LightningElement, api } from 'lwc';
 import { promptInfo, promptError, promptWarning, promptSuccess } from 'c/toasterUtil';
+import { getErrorMessage, logInfo, logError } from 'c/loggingUtil';
 import { customLabels } from 'c/labelLoader';
+
+//Apex methods
+import ctrlSample from '@salesforce/apex/REDU_<%= pascalCaseComponentName %>_LCTRL.sample';
+
 
 export default class <%= pascalCaseComponentName %> extends LightningElement {
 	
@@ -25,35 +30,82 @@ export default class <%= pascalCaseComponentName %> extends LightningElement {
 	//js library module 'lodash', 'stringutil', 'moment', 'fullcalendar', 'fcmoment', 'jquery'
     modules = [];
 	
-	//library loader
+	/**
+     * @descripton library loader
+     */
     handleLibLoadSuccess(event) {
         this.isScriptLoaded = true;
         this.isInitSuccess = true;
         
     }
 
-    //library loader
+    /**
+     * @descripton library loader
+     */
     handleLibLoadFail(event) {
         this.isScriptLoaded = true;
         this.isInitSuccess = false;
     }
 	
+    /**
+     * @descripton rendered callback
+     */
 	renderedCallback(){
 
     }
 
+    /**
+     * @descripton connected callback
+     */
     connectedCallback(){
 		
 	}
 	
+    /**
+     * @descripton disconnected callback
+     */
 	disconnectedCallback() {
 		
 	}
 	
+    /**
+     * @descripton Spinner loading status
+     */
 	get isLoading(){
         return this.loadedLists === 0 ? false : true;
     }
+
+    /**
+     * @descripton Sample method that invoke apex controller
+     */
+    samplePostMethod() {
+        this.toggleSpinner(1);
+
+        try {
+            
+            ctrlSample({
+                
+            })
+            .then(saveResult => {
+                this.toggleSpinner(-1);
+                promptSuccess(this.label.SUCCESS_LABEL, 'any message');
+
+            })
+            .catch(error => {
+                promptError(this.label.ERROR_LABEL, getErrorMessage(error));
+                this.toggleSpinner(-1);
+                
+            })            
+
+        } catch (error) {
+            this.toggleSpinner(-1);
+            promptError(this.label.ERROR_LABEL, getErrorMessage(error));
+        }
+    }
 	
+    /**
+     * @descripton Spinner toggler
+     */
 	toggleSpinner(loadCount){
         this.loadedLists += loadCount;
 
@@ -62,10 +114,11 @@ export default class <%= pascalCaseComponentName %> extends LightningElement {
         }
     }
 	
-	consoleLog(anything){
-        if(this.enableDebugMode){
-            console.log(anything);
-        }
+    /**
+     * @descripton Console log for debugging
+     */
+	consoleLog(anything, isJson){
+        logInfo('jsLibraryLoader', anything, this.enableDebugMode, isJson);
     }
 	
 }
